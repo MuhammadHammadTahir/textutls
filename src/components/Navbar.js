44838c9e5e2f1ef46d6283c4c579f5ab8aeea9fe
MuhammadHammadTahir <mhammadt04@gmail.com>
2023-08-20 20:02:41 +0500

    change link to to

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">{props.title}</a>
+      <link className="navbar-brand" to="/">{props.title}</link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/home">Home</a>
+            <link className="nav-link active" aria-current="page" to="/home">Home</link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/about">{props.about}</a>
+            <link className="nav-link" to="/about">{props.linkbout}</link>
           </li>
         </ul>
         <div className={`form-check form-switch mx-2 text-${props.mode==="dark" ? "light":"dark"}`}>
